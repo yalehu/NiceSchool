3b306571f4b9f24038ee743a212a3fc0c02e80a4
Mr946 <525069428@qq.com>
2019-10-13 02:25:47 +0800

    创建NiceSchoolAPI项目

---
+++ NiceSchoolAPI/src/main/java/com/niceschool/niceschool_api/NiceschoolApiApplication.java
@@ -0,0 +1,13 @@
+package com.niceschool.niceschool_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiceschoolApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NiceschoolApiApplication.class, args);
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# NiceSchool
-NiceSchool教务管理系统
+# NiceSchool教务管理系统
+### 项目构建说明
+
++ NiceSchoolAPI：后端接口
++ NiceSchoolWeb：Web浏览器应用
++ NiceSchoolApplet：小程序（Taro）
++ NiceSchoolRobot：触屏机器端
+
